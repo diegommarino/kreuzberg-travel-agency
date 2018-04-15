c8bdc9f1516ff9df462efd1801389a3f79352593
Diego Marino <diegommarino@gmail.com>
2018-04-15 14:06:28 +0100

    [close #3]_add_nav_and_footer
    - Added the logo image
    - Added the responsive nav component
    - Added the responsive footer component
    - Added vue-awesome, a fontawesome integration with Vue components

---
+++ src/components/navbars/Header.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+div#header
+    nav.navbar.fixed-top.navbar-light.bg-light
+        div#nav-container
+            div#logo-box
+                img#nav-logo(src="@/assets/images/travel-agency-logo.png")
+            div#label-box
+                span#logo-label Travel Agency
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+@media(min-width: 0px) {
+  #nav-container {
+    flex-direction: column;
+  }
+}
+
+@media(min-width: 481px) {
+  #nav-container {
+    flex-direction: row;
+  }
+}
+
+#nav-container {
+  display: flex;
+  max-width: 1260px;
+  margin: auto;
+}
+
+#logo-box {
+  text-align: center;
+}
+
+#nav-logo {
+  width: 6rem;
+}
+
+#label-box {
+  text-align: center;
+  margin: 20px;
+}
+
+#logo-label {
+  font-family: "Pacifico";
+  font-size: 2.2rem;
+}
+</style>
